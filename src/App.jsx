688ffc24bc53bb44c5448d83dc6dcb487580959a
SHIVAMCP18 <shivampatel180905@gmail.com>
2025-06-28 12:07:31 +0530

    initial commit of gaming website

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Features from "./components/Features.jsx";
+import Story from "./components/Story.jsx";
+import Contact from "./components/Contact.jsx";
+
+const App = () => {
+    return (
+        <main className="relative min-h-screen w-screen overflow-x-hidden ">
+            <Navbar />
+            <Hero />
+            <About />
+            <Features />
+            <Story />
+            <Contact />
+        </main>
+    )
+}
+export default App
